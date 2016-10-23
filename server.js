67eaa6af8ca1a05ef6532bb7a0685f3c12ff7823
jonas <jonas.waldeen@gmail.com>
2016-10-23 16:09:24 +0200

    offline fix for mongoose

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const routes = require('./app/routes');
 const {init: setupHandlebars} = require('./app/setup/handlebars');
 const app = express();
 const port = process.env.PORT || 3000;
 
-mongoose.connect('mongodb://localhost:27017/countdown');
+mongoose.connect('mongodb://127.0.0.1:27017/countdown');
 
 app.use(express.static(`${__dirname}/public`));
-app.use(require('./app/routes'));
+app.use(routes);
 
 setupHandlebars(app);
 
