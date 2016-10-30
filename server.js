c41a52f29be21563b457a744df8671efb56485d8
jonas <jonas.waldeen@gmail.com>
2016-10-30 17:24:18 +0100

    added packages to package.json, added helmet to application

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const helmet = require('helmet');
 const routes = require('./app/routes');
 const {init: setupHandlebars} = require('./app/setup/handlebars');
 const app = express();
@@ -8,6 +9,7 @@ const port = process.env.PORT || 3000;
 mongoose.connect('mongodb://127.0.0.1:27017/countdown');
 
 app.use(express.static(`${__dirname}/public`));
+app.use(helmet());
 app.use(routes);
 
 setupHandlebars(app);
