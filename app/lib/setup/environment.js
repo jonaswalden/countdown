5d28a1244678a026403416d6ea3b74f73eedc993
jonas <jonas.waldeen@gmail.com>
2017-08-02 21:16:23 +0200

    don't override existing environment

--- app/lib/setup/environment.js
+++ app/lib/setup/environment.js
@@ -3,6 +3,7 @@
 module.exports = setup;
 
 function setup (environment) {
+	if (process.env.NODE_ENV) return;
 	process.env.NODE_ENV = environment || 'development';
 	process.env.TZ = 'Europe/Stockholm';
 }
