fd7cf6cfb099f9516cc495dddff618997e8b156e
jonas <jonas.waldeen@gmail.com>
2017-08-17 23:40:24 +0200

    serving bundles and including them

--- app/lib/setup/middleware.js
+++ app/lib/setup/middleware.js
@@ -7,7 +7,8 @@ const methodOverride = require('method-override');
 module.exports = setup;
 
 function setup (app) {
-	app.use('/assets', express.static('app/assets'));
+	app.use('/bundles', express.static('resources/bundles'));
+	app.use('/images', express.static('resources/images'));
 	app.use(helmet());
 	app.use(methodOverride('_method'));
 }
