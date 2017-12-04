011ff3c7461bb419db8ad2c61816b7b193b60925
jonas <jonas.waldeen@gmail.com>
2017-12-05 00:19:18 +0100

    wip - testing inline formatting

--- test/app/lib/helpers/font-query.test.js
+++ test/app/lib/helpers/font-query.test.js
@@ -3,46 +3,95 @@
 const fontQuery = require('../../../../app/lib/helpers/font-query');
 
 describe('fontQuery', () => {
-	const basicText = '' +
-		'# Event name yo!\n' +
-		'\n' +
-		'Event description with\n' +
-		'describing words and such' +
-		'';
+	describe('basic text', () => {
+		const text = '' +
+			'# Event name yo!\n' +
+			'\n' +
+			'Event description with\n' +
+			'describing words and such' +
+			'';
 
-	it('combines multiple rules to a query', () => {
-		const bodyFont = '200 24px/1.4 "PT Sans", sans-serif';
-		const headingFont = '900 50px/1 "Merriweather", serif';
+		it('combines multiple rules to a query', () => {
+			const bodyFont = '200 24px/1.4 "PT Sans", sans-serif';
+			const headingFont = '700 50px/1 "Merriweather", serif';
 
-		const query = fontQuery(basicText, bodyFont, headingFont);
+			const query = fontQuery(text, bodyFont, headingFont);
 
-		expect(query).to.equal('family=PT%20Sans:200|Merriweather:900');
+			expect(query).to.equal('family=PT%20Sans:200|Merriweather:700');
+		});
+
+		it('disregards rules not using a custom font', () => {
+			const bodyFont = '200 24px/1.4 sans-serif';
+			const headingFont = '700 50px/1 "Merriweather", serif';
+
+			const query = fontQuery(text, bodyFont, headingFont);
+
+			expect(query).to.equal('family=Merriweather:700');
+		});
+
+		it('explicitly uses weight 400 as default if combined with other variations', () => {
+			const bodyFont = '24px/1.4 "PT Sans", sans-serif';
+			const headingFont = '700 50px/1 "PT Sans", sans-serif';
+
+			const query = fontQuery(text, bodyFont, headingFont);
+
+			expect(query).to.equal('family=PT%20Sans:400,700');
+		});
+
+		it('merges multiple variations of the same family', () => {
+			const bodyFont = '200 24px/1.4 "PT Sans", sans-serif';
+			const headingFont = '700 50px/1 "PT Sans", sans-serif';
+
+			const query = fontQuery(text, bodyFont, headingFont);
+
+			expect(query).to.equal('family=PT%20Sans:200,700');
+		});
 	});
 
-	it('disregards rules not using a custom font', () => {
-		const bodyFont = '200 24px/1.4 sans-serif';
-		const headingFont = '900 50px/1 "Merriweather", serif';
+	describe('text with inline formatting', () => {
+		const text = '' +
+			'# Event _name_ __yo__!\n' +
+			'\n' +
+			'**Strong as phony heading**' +
+			'\n' +
+			'Event description with\n' +
+			'*emphasized words* and such' +
+			'';
 
-		const query = fontQuery(basicText, bodyFont, headingFont);
+		it('combines multiple rules to a query', () => {
+			const bodyFont = '200 24px/1.4 "PT Sans", sans-serif';
+			const headingFont = '700 50px/1 "Merriweather", serif';
 
-		expect(query).to.equal('family=Merriweather:900');
-	});
+			const query = fontQuery(text, bodyFont, headingFont);
 
-	it('explicitly uses weight 400 as default if combined with other variations', () => {
-		const bodyFont = '24px/1.4 "PT Sans", sans-serif';
-		const headingFont = '700 50px/1 "PT Sans", sans-serif';
+			expect(query).to.equal('family=PT%20Sans:200|Merriweather:700,700italic,900');
+		});
 
-		const query = fontQuery(basicText, bodyFont, headingFont);
+		it('disregards rules not using a custom font', () => {
+			const bodyFont = '200 24px/1.4 sans-serif';
+			const headingFont = '900 50px/1 "Merriweather", serif';
 
-		expect(query).to.equal('family=PT%20Sans:400,700');
-	});
+			const query = fontQuery(text, bodyFont, headingFont);
 
-	it('merges multiple variations of the same family', () => {
-		const bodyFont = '200 24px/1.4 "PT Sans", sans-serif';
-		const headingFont = '900 50px/1 "PT Sans", sans-serif';
+			expect(query).to.equal('family=Merriweather:900');
+		});
 
-		const query = fontQuery(basicText, bodyFont, headingFont);
+		it('explicitly uses weight 400 as default if combined with other variations', () => {
+			const bodyFont = '24px/1.4 "PT Sans", sans-serif';
+			const headingFont = '700 50px/1 "PT Sans", sans-serif';
 
-		expect(query).to.equal('family=PT%20Sans:200,900');
+			const query = fontQuery(text, bodyFont, headingFont);
+
+			expect(query).to.equal('family=PT%20Sans:400,700');
+		});
+
+		it('merges multiple variations of the same family', () => {
+			const bodyFont = '200 24px/1.4 "PT Sans", sans-serif';
+			const headingFont = '900 50px/1 "PT Sans", sans-serif';
+
+			const query = fontQuery(text, bodyFont, headingFont);
+
+			expect(query).to.equal('family=PT%20Sans:200,900');
+		});
 	});
 });
