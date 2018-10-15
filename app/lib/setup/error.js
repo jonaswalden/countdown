37e708846f3c54d82439ce98df595047a4734628
jonaswalden <jonas.waldeen@gmail.com>
2018-10-15 23:19:27 +0200

    updates to error handler

--- app/lib/setup/error.js
+++ app/lib/setup/error.js
@@ -9,9 +9,10 @@ module.exports = function setup (app) {
 function handleError (err, req, res, next) {
 	if (!err) return next(err);
 
+	const status = err.status || 500;
 	logger.error(err);
-	res.send(500, `
-		<h1>Oh noes!</h1>
+	res.status(status).send(`
+		<h1>${status} Oh noes!</h1>
 		<h2>${err.message}</h2>
 		<pre>${err.trace}</pre>
 	`);
